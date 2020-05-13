706b87dd66e69ed030ea3b644027919ace84ab11
Ankita-Sihag <ankitasihag2001@gmail.com>
2020-05-13 16:47:58 +0530

    Updates

---
+++ precache-manifest.882dc597edd7c71756b6b54d2ed538c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034a16a420088540d0b0f92231a009f7",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "06594238bdec00790565",
+    "url": "/tic-tac-toe/static/css/main.42a7088e.chunk.css"
+  },
+  {
+    "revision": "66ea6ec2173f9f796107",
+    "url": "/tic-tac-toe/static/js/2.e7c78e37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.e7c78e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06594238bdec00790565",
+    "url": "/tic-tac-toe/static/js/main.169ca3fe.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
